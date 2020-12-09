8c2b38781a8d29873b68d675471b87bf42c3323d
xingren2020 <64955847+xingren2020@users.noreply.github.com>
2020-12-09 13:46:14 +0800

    Update qdCookie.js

--- qdCookie.js
+++ qdCookie.js
@@ -2,14 +2,15 @@ let CookieQDs = [];
 
 // 判断github action里面是否有京东ck
 if (process.env.QD_COOKIE) {
-  if (process.env.QD_COOKIE.indexOf('--') > -1) {
-    console.log(`您的cookie选择的是用--隔开\n`)
-    CookieQDs = process.env.QD_COOKIE.split('--');
-  } else (process.env.QD_COOKIE.indexOf('&&') > -1) {
-    console.log(`您的cookie选择的是用&&隔开\n`)
-    CookieQDs = process.env.QD_COOKIE.split('&&');
-  }
+    if (process.env.QD_COOKIE.indexOf('--') > -1) {
+        console.log(`您的cookie选择的是用--隔开\n`)
+        CookieQDs = process.env.QD_COOKIE.split('--');
+    } else if (process.env.QD_COOKIE.indexOf('&&') > -1){
+        console.log(`您的cookie选择的是用&&隔开\n`)
+        CookieQDs = process.env.QD_COOKIE.split('&&');
+    }
 }
+
 for (let i = 0; i < CookieQDs.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['CookieQD' + index] = CookieQDs[i];
